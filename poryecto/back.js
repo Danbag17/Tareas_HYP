7169ac11cff5fb008106a6bcc05f76651e7e0a76
Danbag17 <danibany18@gmail.com>
2026-02-10 12:27:52 +0100

    feat: Refactor lógica de tareas y mejorar la estructura del HTML y CSS

--- poryecto/back.js
+++ poryecto/back.js
@@ -40,13 +40,11 @@ class tarea{
 const tareas=[];
 //document.getElementById("lista_tarea").innerText="Tareas: " + tareas[0].getTexto ;
 
-const input = document.getElementById("texto");
+const input_texto = document.getElementById("texto");
+const input_responsable = document.getElementById("responsable");
 const boton = document.getElementById("boton");
 const resultado = document.getElementById("resultado");
-
- 
-
-  const lista = document.getElementById("lista");
+const lista = document.getElementById("lista");
     
   boton.addEventListener("click", () => {
     
@@ -54,13 +52,12 @@ const resultado = document.getElementById("resultado");
     });
 
 
-function agregarTarea(){
-    const texto = document.getElementById("texto").value;
-    const responsable = document.getElementById("responsable").value;
+function agregarTarea_lista(){
+    const texto = input_texto.value;
+    const responsable = input_responsable.value;
     const nombre = "Tarea " + (tareas.length + 1);
     try {
-        const tareaNueva = new tarea(nombre, texto, responsable);
-        tareas.push(tareaNueva);
+        
         const li = document.createElement("li");
         li.textContent = tareaNueva.getTexto;
         lista.appendChild(li);
@@ -71,15 +68,25 @@ function agregarTarea(){
 }
 
 function agregarTarea(){
-    const texto = document.getElementById("texto").value;
-    const responsable = document.getElementById("responsable").value;
+    const texto = input_texto.value;
+    const responsable = input_responsable.value;
     const nombre = "Tarea " + (tareas.length + 1);
     try {
         const tareaNueva = new tarea(nombre, texto, responsable);
+        tareas.push(tareaNueva);
         const tr = document.createElement("tr");
         const td = document.createElement("td");
         td.textContent = tareaNueva.getTexto;
+        const td2 = document.createElement("td");
+        td2.textContent = tareaNueva.getResponsable;
+        const td3 = document.createElement("td");
+        td3.textContent = tareaNueva.nombre;
+        //nombre
+        tr.appendChild(td3);
+        //texto
         tr.appendChild(td);
+        //responsable
+        tr.appendChild(td2);
         lista.appendChild(tr);
     }   
     catch (error) {
